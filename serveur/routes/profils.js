8e04d2aec1d0e537061c2e82f32d7e4cfdf67198
hadidi09 <cheikhhadramy@yahoo.fr>
2021-06-18 17:12:16 +0200

    first test form data

--- serveur/routes/profils.js
+++ serveur/routes/profils.js
@@ -1,12 +1,13 @@
 const express = require("express")
 const router = express.Router()
-
 const profilControllers = require("../controllers/profils")
 const auth = require('../middlewares/jsonwebtoken')
-const multer = require('../middlewares/multer-config')
+const upload = require('../middlewares/multer-config')
+// console.log(upload);
+// 
 
-router.post("/images" , multer,profilControllers.imagePost)
+router.post("/images" , upload.single('image'), /* profilControllers.imagePost */)
 router.get("/profil/:id", auth,profilControllers.profils)
 
 
-module.exports = router;
+module.exports = router;  
